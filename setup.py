b03edcb05d76084a7207392a4f79469afd5463e3
Nyloner <nyloner.root@gmail.com>
2017-10-25 11:16:09 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='trans',
-	version='1.0.0',
-	author='Nyloner',
-	author_email='nyloner.root@gmail.com',
+    name='trans',
+    version='1.0.0',
+    author='Nyloner',
+    author_email='nyloner.root@gmail.com',
     url='https://github.com/Nyloner/trans',
-	description='A simple tool for translation.',
-	license='BSD',
-	packages=find_packages(),
-	install_requires=['requests', 'bs4', 'lxml'],
-	entry_points= {
-        'console_scripts': [
-            'trans = trans.trans:execute'
-        ]
-    }
+   	description='A Simple Translation Tool In Your Command Line.',
+   	license='BSD',
+   	packages=find_packages(),
+   	install_requires=['requests', 'bs4', 'lxml'],
+   	entry_points={
+            'console_scripts': [
+                'trans = trans.trans:execute'
+            ]
+        }
 );
